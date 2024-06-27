879acfe2ab0e79fe94d67b2c157e0793cad37613
No one knows <142857872+unknownbuddy1@users.noreply.github.com>
2024-06-27 15:28:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='footer-container'>
         <p>
-          © 2023 <strong>Adrian Hajdin</strong>. All rights reserved.
+          © 2023 <strong>𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪</strong>. All rights reserved.
         </p>
 
         <div className='flex gap-3 justify-center items-center'>
